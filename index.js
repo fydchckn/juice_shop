26f2ba4a8c5bf5909032b55e874bd8b7c4392201
fydchckn <kentfamulagan283@gmail.com>
2024-09-06 22:05:51 +0800

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const header = document.querySelector('.primary-header');
 
 
 navtoggle.addEventListener('click', () => {
-    const expanded = navtoggle.getAttribute("aria-expanded") === "true"
+    const expanded = navtoggle.getAttribute("aria-expanded") === "true";
     navtoggle.setAttribute("aria-expanded", !expanded);
 
     mynav.toggleAttribute('nav-open');
